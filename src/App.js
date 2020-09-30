1c181277b47a77d9341d7d68c8e2a813f73bf1cb
Boogle Staff <melvintseng@BoogleStaff.local>
2020-09-30 16:18:04 +0800

    Axious + fetch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import Pokedex from "./Pokedex";
+import Pokemon from "./Pokemon";
+import { BrowserRouter as Router ,Route, Switch } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <h2>hi</h2>
-    </div>
-  );
-}
+const App = () => (
+  <Router>
+  <Switch>
+    <Route exact path="/" render={(props) => <Pokedex {...props} />} />
+    <Route
+      exact
+      path="/:pokemonId"
+      render={(props) => <Pokemon {...props} />}
+    />
+  </Switch>
+  </Router>
+);
 
 export default App;
